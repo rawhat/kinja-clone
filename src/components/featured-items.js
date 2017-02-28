9bd91f27398b631321ad87d2db0c6d596e546768
alex manning <alex.w.manning@gmail.com>
2017-02-28 13:32:42 -0500

    got most of the basic structure/css done

--- src/components/featured-items.js
+++ src/components/featured-items.js
@@ -1,32 +1,66 @@
-import React from 'react';
+import React, { Component } from 'react';
+import _ from 'lodash';
 
-export default () => {
-    return (
-        <div className="featured-items">
-            <div className="featured-item">
-                <div className='featured-item-image'>
-                    <img src='http://i.imgur.com/VSnfDuz.jpg' />
-                    <div className="featured-item-header">This is test image 1.</div>
-                </div>                
-            </div>
-            <div className="featured-item">
-                <div className='featured-item-image'>
-                    <img src='http://i.imgur.com/VSnfDuz.jpg' />
-                    <div className="featured-item-header">This is test image 2.</div>
+export default class FeaturedItems extends Component {
+    static state = {
+        windowWidth: -1,
+        featuredItemWidth: -1
+    };
+
+    componentWillMount = () => {
+        this.setState({
+            windowWidth: window.innerWidth
+        });
+    }
+
+    componentDidMount = () => {
+        this.windowSizeListener = window.addEventListener('resize', _.debounce(() => {
+            this.setState({
+                windowWidth: window.innerWidth,
+            });
+        }, 50));
+    }
+
+    componentWillUnmount = () => {
+        window.removeEventListener(this.windowSizeListener);
+    }
+
+    render = () => {
+        // let windowWidth = window.innerWidth;
+        // let windowHeight = window.innerHeight;
+
+        let imgStyle = {
+            width: (this.state.windowWidth - 175) / 4,
+            height: 'auto'//(windowHeight - 250) / 4
+        };
+
+        return (
+            <div className="featured-items">
+                <div className="featured-item">
+                    <div className='featured-item-image'>
+                        <img src='http://i.imgur.com/VSnfDuz.jpg' style={imgStyle}/>
+                        <div className="featured-item-header" style={{ left: 3 }}>This is test image 1.</div>
+                    </div>                
+                </div>
+                <div className="featured-item">
+                    <div className='featured-item-image'>
+                        <img src='http://i.imgur.com/VSnfDuz.jpg' style={imgStyle}/>
+                        <div className="featured-item-header" style={{ left: 10 }}>This is test image 2.</div>
+                    </div>
+                </div>
+                <div className="featured-item">
+                    <div className='featured-item-image'>
+                        <img src='http://i.imgur.com/VSnfDuz.jpg' style={imgStyle}/>
+                        <div className="featured-item-header" style={{ left: 10 }}>This is test image 3.</div>
+                    </div>
+                </div>
+                <div className="featured-item">
+                    <div className='featured-item-image'>
+                        <img src='http://i.imgur.com/VSnfDuz.jpg' style={imgStyle}/>
+                        <div className="featured-item-header" style={{ left: 3 }}>This is test image 4.</div>
+                    </div>
                 </div>
             </div>
-            <div className="featured-item">
-                <div className='featured-item-image'>
-                    <img src='http://i.imgur.com/VSnfDuz.jpg' />
-                    <div className="featured-item-header">This is test image 3.</div>
-                </div>
-            </div>
-            <div className="featured-item">
-                <div className='featured-item-image'>
-                    <img src='http://i.imgur.com/VSnfDuz.jpg' />
-                    <div className="featured-item-header">This is test image 4.</div>
-                </div>
-            </div>
-        </div>
-    );
+        );
+    }
 }
--- src/components/main-body-content.js
+++ src/components/main-body-content.js
@@ -1,7 +1,79 @@
 import React from 'react';
 
-export default () => {
+const MainBody = () => {
+    let mockData = [
+        {
+            id: 0,
+            sharedFrom: 'Deadspin',
+            title: 'Mikael Granlund Beats All The Kings For Beautiful OT Winner',
+            author: 'Billy Haisley',
+            postedAt: Date.now() - 17000,
+            contentType: 'image',
+            contentUrl: 'http://i.imgur.com/VSnfDuz.jpg',
+            textIntro: 'There were a whole lot of storylines heading into Monday’s clash between the West-leading Wild and the just-outside-the-playoff-picture Kings. The day before, both teams had recruited reinforcements for their respective postseason pushes: L.A. with a surprise trade for goalie Ben Bishop, and Minnesota acquiring Martin…',
+            recommendations: 8,
+            comments: 30
+        },
+        {
+            id: 1,
+            sharedFrom: 'Deadspin',
+            title: 'As Expected, The New Vikings Stadium Is Killing A Record Number Of Birds',
+            author: 'Barry Petchesky',
+            postedAt: Date.now() - (1000 * 60 * 60 * 2),
+            contentType: 'image',
+            contentUrl: 'http://i.imgur.com/VSnfDuz.jpg',
+            textIntro: 'No one can say they didn’t see this coming. Well, the birds can.',
+            recommendations: 4,
+            comments: 13
+        }
+    ];
+
     return (
-        <div className='main-body-content'></div>
+        <div className='main-body-content'>
+            {mockData.map(story => {
+                let mediaTag = null;
+                switch(story.contentType) {
+                    case 'image': {
+                        mediaTag = <img src={story.contentUrl} />;
+                        break;
+                    }
+                    case 'video': {
+                        break;
+                    }
+                }
+
+                return <div className='content-area'> 
+                    <div className='content-section' key={story.id}>
+                        {story.sharedFrom ? <div className='shared-from'>
+                            <i className="material-icons md-dark">sync</i>
+                            <span>Shared from {story.sharedFrom}</span>
+                        </div> : null}
+                        <h4>{story.title}</h4>
+                        <div>
+                            <h6 style={{ display: 'inline-block' }}>{story.author}</h6>
+                            {' - '}
+                            <h6 style={{ display: 'inline-block' }}><small>{new Date(story.postedAt).toLocaleString()}</small></h6>
+                        </div>
+                        {mediaTag}
+                        <p>{story.textIntro}</p>
+                    </div>
+                    <div className='content-sidebar'>
+                        <div className='icon'>
+                            <i className="material-icons md-dark" style={{ width: 36, fontSize: '36px'}}>star rate</i>
+                        </div>
+                        <span className='icon-text'>{story.recommendations}</span>
+                        <div className='icon'>
+                            <i className="material-icons md-dark" style={{ width: 36, fontSize: '36px'}}>chat bubble outline</i>
+                        </div>
+                        <span className='icon-text'>{story.comments}</span>
+                        <div className='icon'>
+                            <i className="material-icons md-dark" style={{ width: 36, fontSize: '36px'}}>more vert</i>
+                        </div>
+                    </div>
+                </div>;
+            })}
+        </div>
     );
-}
+};
+
+export default MainBody;
